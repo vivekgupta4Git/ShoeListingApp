0235b1f5c9e832f928988edfa698e1a9b4c105c3
Vivek <itguru4all@gmail.com>
2021-10-19 14:17:28 +0530

    Added overflow menu

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,21 +2,29 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.models.ShoeViewModel
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //planting a tree for debugging
         Timber.plant(Timber.DebugTree())
+
+        //find a nav Controller
         val navController = this.findNavController(R.id.myNavHostFragment)
 
+        //Configuration AppBar using navigation graph
         val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        //hooking up ActionBar using Nav Controller with appBarConfiguration
         NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration)
 
 
--- app/src/main/java/com/udacity/shoestore/screens/ShoeListingFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/ShoeListingFragment.kt
@@ -1,16 +1,17 @@
 package com.udacity.shoestore.screens
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.marginLeft
 import androidx.core.view.marginRight
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoelistingBinding
 import com.udacity.shoestore.models.Shoe
@@ -20,31 +21,25 @@ import org.w3c.dom.Text
 class ShoeListingFragment : Fragment() {
 
     private lateinit var binding: FragmentShoelistingBinding
-    private lateinit var viewModel: ShoeViewModel
             override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+         savedInstanceState: Bundle?
     ): View? {
                 //inflating layout
         binding = DataBindingUtil.inflate(inflater,
         R.layout.fragment_shoelisting,
         container,false)
 
-                viewModel = ViewModelProvider(this).get(ShoeViewModel::class.java)
+                setHasOptionsMenu(true)
 
-
-
-        return binding.root
+       return binding.root
             }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-        //    binding.LinearLayoutList.addView()
-        val viewModel =ViewModelProvider(this).get(ShoeViewModel::class.java)
-
+//        use an Activity level ViewModel to hold a list of Shoes (use by activityViewModels)
+     val   viewModel: ShoeViewModel by activityViewModels()
 
         for(shoe in viewModel.shoeList.value!!)
         {
@@ -55,20 +50,33 @@ class ShoeListingFragment : Fragment() {
 
     }
 
+
+
     private fun linearViewForShoe(shoe: Shoe): LinearLayout {
+        //initializing variable of linearlayout
         val linearLayout  = LinearLayout(context)
 
-
+        //creating parameters for layout
         val layoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT
         )
-
+        //16dp margin for linearlayout
         layoutParams.setMargins(16,16,16,16)
 
-
+        //adding this params to linearlayout
         linearLayout.layoutParams =layoutParams
+
+        /*
+        1. A new item layout is created for each item
+        2. New item layout is added to LinearLayout
+        3. Layout is updated with items added on the detail screen
+
+         */
+
+        //setting orientation to horizontal to show a single shoe detail horizontally i.e column wise
         linearLayout.orientation = LinearLayout.HORIZONTAL
 
+        //creating a textview , setting it params and setting its text value
         val shoeNameText = TextView(context)
         shoeNameText.layoutParams = layoutParams
         shoeNameText.text = shoe.name
@@ -93,9 +101,16 @@ class ShoeListingFragment : Fragment() {
 
         return linearLayout
 
+    }
 
-
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu,menu)
     }
 
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 }
